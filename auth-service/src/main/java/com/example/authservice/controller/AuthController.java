2235484461f03331eaa50f332ce509796dce4eaa
yash55-star <yash.divecha555@gmail.com>
2026-03-11 12:18:25 +0530

    testing CI pipeline

--- auth-service/src/main/java/com/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(GlobalConstants.BASE_REST_URL + "/auth")
 public class AuthController {
+    
 
     private final AppUserService appUserService;
 
